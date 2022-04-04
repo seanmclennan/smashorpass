eb23af2b9276357e86702c864c662795f421176b
danilevii <daniellevicoding@hotmail.com>
2022-04-03 17:32:15 -0700

    Initial commit

--- src/server/app.js
+++ src/server/app.js
@@ -1,1 +1,13 @@
-const expr
+const express = require('express')
+const app = express();
+require('dotenv').config()
+
+app.get('/', (req, res) => {
+    res.send("Smash or Pass")
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, () => {
+    console.log(`Server is listening on port ${PORT}...`)
+})
